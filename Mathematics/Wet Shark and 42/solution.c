703a7c75aa7bdb5f6561ef8cb66c19df89d0e39b
Jack Rus <JackRus@users.noreply.github.com>
2017-03-12 15:45:14 -0400

    Passes all tests

---
+++ Mathematics/Wet Shark and 42/solution.c
@@ -0,0 +1,23 @@
+/*
+*   Author: Jack Rus
+*
+*   03/2017
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+    int N;
+    long long a;
+    scanf("%i", &N);
+    for (int i = 1; i <= N; i++)
+    {
+        scanf("%lld", &a);
+        a = a % 20 != 0 ? (a / 20 * 42 % 1000000007 + 2 * (a % 20)) : (a / 20 * 42 % 1000000007 - 2);
+        printf("%lld\n", a);
+    }
+}
