4c5636599a31cba6100c8e6b2aea86e67e7d1fe9
Jack Rus <JackRus@users.noreply.github.com>
2017-03-24 14:00:50 -0400

    Update solution.c

--- Data Structures/Left Rotation/solution.c
+++ Data Structures/Left Rotation/solution.c
@@ -16,9 +16,9 @@ int main()
     int n, d; 
     scanf("%d %d", &n, &d);
     int arr[n];
-    for(int arr_i = 0; arr_i < n; arr_i++){
+    for(int arr_i = 0; arr_i < n; arr_i++)
         scanf("%d",&arr[arr_i]);
-    }
+    
     d = d % n;
     int sub;
     // moves to the left
@@ -28,9 +28,7 @@ int main()
         {
             sub = arr[0];
             for (int j = 1; j < n; j++)
-            {
                 arr[j-1] = arr[j];
-            }
             arr[n-1] = sub;
         }
     } 
@@ -41,15 +39,11 @@ int main()
         {
             sub = arr[n-1];
             for (int j = n -1; j > 0; j--)
-            {
                 arr[j] = arr[j-1];
-            }
             arr[0] = sub;
         }
     }
     for (int i = 0; i < n; i++)
-    {
         printf("%i ", arr[i]);
-    }
     return 0;
 }
