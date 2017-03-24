c159345fc875c8c5638ebbe1089a45a0cb8be0da
Jack Rus <JackRus@users.noreply.github.com>
2017-03-24 14:02:45 -0400

    Update solution.c

--- Mathematics/Wet Shark and 42/solution.c
+++ Mathematics/Wet Shark and 42/solution.c
@@ -14,7 +14,7 @@ int main(void)
     int N;
     long long a;
     scanf("%i", &N);
-    for (int i = 1; i <= N; i++)
+    for (int i = 0; i < N; i++)
     {
         scanf("%lld", &a);
         a = a % 20 != 0 ? (a / 20 * 42 % 1000000007 + 2 * (a % 20)) : (a / 20 * 42 % 1000000007 - 2);
